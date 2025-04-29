49ca9aa064ece475957d6270a83f83fb1a93b5b1
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-04-28 23:57:15 -0300

    Métodos etáticos

---
+++ 7_metodo_estatico.py
@@ -0,0 +1,34 @@
+class Pessoa:
+    possui_olho = True
+    possui_boca = True
+    raca = 'Ser humano'
+    
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def retorna_nome(self):
+        return self.nome
+    
+    def logar_sistema(self):
+        print(f'{self.retorna_nome()} Está logado no sistema.')
+
+    @classmethod
+    def andar(cls):
+        cls.pernas = 2
+        return None
+
+    @staticmethod
+    def e_adulto(idade):
+        if idade >= 18:
+            return True
+        return False
+# p1 = Pessoa('Janio', 39)
+# p2 = Pessoa('Alexandre', 30)
+
+# Pessoa.andar(10)
+
+Pessoa.andar()
+print(Pessoa.pernas)
+
+print(Pessoa.e_adulto(18))
